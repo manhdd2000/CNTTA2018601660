aba7e44c1c1ab15a1fa9513c36cd9e2d7fc3228b
manhdd2000 <78583128+manhdd2000@users.noreply.github.com>
2021-05-26 00:48:43 +0700

    Add files via upload

---
+++ MoDongCong.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoDongCong : MonoBehaviour
+{
+    public GameObject CanhCong;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            CanhCong.GetComponent<Animation>().Play("QuayCong");
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            CanhCong.GetComponent<Animation>().Play("DongCong");
+        }
+    }
+}
