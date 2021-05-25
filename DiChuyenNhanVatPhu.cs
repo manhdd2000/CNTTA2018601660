47bbff94fbe728e80f8f11fce4e28bd4eb11384d
manhdd2000 <78583128+manhdd2000@users.noreply.github.com>
2021-05-26 00:44:07 +0700

    Add files via upload

---
+++ DiChuyenNhanVatPhu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DiChuyenNhanVatPhu : MonoBehaviour
+{
+
+	public GameObject desinationPoint;
+	NavMeshAgent theAgent;
+
+	void Start()
+	{
+		theAgent = GetComponent<NavMeshAgent>();
+	}
+
+
+
+	void Update()
+	{
+		theAgent.SetDestination(desinationPoint.transform.position);
+	}
+}
